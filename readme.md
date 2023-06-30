29346ad969fcaf0b5b84ef86d2a6c1c990e4b749
Chris Bao <cbao23@concordcarlisle.org>
2023-06-29 21:02:47 -0400

    added ready button!

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Durak
+
+A personal project for education and entertainment ... edutainment?
+
+*Durak* is a multiplayer card game ([more info](https://en.wikipedia.org/wiki/Durak)) that I'm creating an app version for. The aim is to rid one's hand of all cards; the last one left with cards in their hands is *durak* ("fool" in Russian).
+
+## Planned features
+
+- Client-server capability
+- Multiplayer — up to 6 people may join
+- All-custom art and assets
+
+© Chris Bao 2023.
--- src/client.py
+++ src/client.py
@@ -16,81 +16,13 @@ import sys
 # Internal imports
 from game import Player
 
+pygame.freetype.init()
+
 
 class Client:
     """
     `Client` class. Provides user interface and network functionality.
     """
-
-    class Button:
-        """
-        `Button` class for the UI.
-        """
-
-        BUTTON_FONT: pygame.freetype.Font = pygame.freetype.Font(
-            "../res/font/robotoRegular.ttf", 48)
-
-        def __init__(self, text: str, pos: tuple[int, int], size: tuple[int, int],
-                     fg: tuple[int, int, int], bg: tuple[int, int, int],
-                     font: pygame.freetype.Font) -> None:
-            """
-            Constructor.
-
-            Parameters
-            ---
-            `text: str` - button text.
-            `pos: tuple[int, int]` - position of the button on the window.
-            `size: tuple[int, int]` - width and height.
-            `fg: tuple[int, int, int]` - RGB color of the text.
-            `bg: tuple[int, int, int]` - RGB color of the background.
-
-            Returns
-            ---
-            `None`
-            """
-            self.text: str = text
-            self.pos: tuple[int, int] = pos
-            self.size: tuple[int, int] = size
-            self.fg: tuple[int, int, int] = fg
-            self.bg: tuple[int, int, int] = bg
-            self.font: pygame.font.Font = font
-
-        def draw(self, window: pygame.Surface):
-            """
-            Draw this button to the specified window.
-
-            Parameters
-            ---
-            `window: pygame.Surface` - window to draw on.
-
-            Returns
-            ---
-            `None`
-            """
-            # TODO: check this works
-            button_rect = pygame.draw.rect(
-                window, self.bg, self.pos + self.size)
-            text_surface = self.font.render(self.text, True, self.fg)
-            text_rect = text_surface.get_rect(center=button_rect.center)
-            window.blit(text_surface, text_rect)
-
-        def check_clicked(self, mouse_x: int, mouse_y: int) -> bool:
-            """
-            Check if this button has been clicked given a mouse position.
-
-            Parameters
-            ---
-            `mouse_x: int`
-            `mouse_y`: int`
-
-            Returns
-            ---
-            `bool` - `True` if clicked, `False` otherwise
-            """
-            return (self.pos[0] <= mouse_x <= self.pos[0] + self.size[0] and
-                    self.pos[1] <= mouse_y <= self.pos[1] + self.size[1])
-
-        # TODO: add and check button
 
     ### Constants ###
     # For IO
@@ -104,6 +36,13 @@ class Client:
     # For UI
     WINDOW_WIDTH: int = 1440
     WINDOW_HEIGHT: int = 900
+
+    BG_COLOR: tuple[int, int, int] = (0, 0, 0)
+    BTN_BG_COLOR: tuple[int, int, int] = (255, 255, 255)
+    BTN_FG_COLOR: tuple[int, int, int] = (0, 0, 0)
+
+    READY_BTN_WIDTH: int = 200
+    READY_BTN_HEIGHT: int = 80
 
     ### Instance variables ###
     socket: s.socket
@@ -136,6 +75,14 @@ class Client:
         self.window: pygame.Surface = pygame.display.set_mode(
             (Client.WINDOW_WIDTH, Client.WINDOW_HEIGHT))
         self.clock: pygame.time.Clock = pygame.time.Clock()
+
+        self.button = self.Button("Ready!",
+                                  (self.WINDOW_WIDTH/2 - self.READY_BTN_WIDTH/2,
+                                   self.WINDOW_HEIGHT/2 - self.READY_BTN_HEIGHT/2),
+                                  (self.READY_BTN_WIDTH, self.READY_BTN_HEIGHT),
+                                  self.BTN_FG_COLOR,
+                                  self.BTN_BG_COLOR,
+                                  self.Button.BUTTON_FONT)
 
     def connect(self) -> str:
         """
@@ -173,6 +120,31 @@ class Client:
         except s.error as e:
             print(e)
 
+    def draw(self) -> None:
+        """
+        Draw everything to the window.
+
+        Parameters
+        ---
+        (no parameters)
+
+        Returns
+        ---
+        `None`
+        """
+        self.window.fill(Client.BG_COLOR)
+        self.button.draw(self.window)
+
+        # use the correct cursor
+        # selecting button
+        if self.button.rect.collidepoint(pygame.mouse.get_pos()):
+            pygame.mouse.set_cursor(
+                pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_HAND))
+        # default arrow
+        else:
+            pygame.mouse.set_cursor(
+                pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_ARROW))
+
     def mainloop(self) -> None:
         """
         Mainloop. Handle all events while the app is running.
@@ -186,19 +158,110 @@ class Client:
         `None`
         """
         while True:
+            message = "refresh"
+
             # handle events
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
 
+                if event.type == MOUSEBUTTONUP:
+                    if self.button.check_clicked():
+                        message = "ready"
+
             # user_input = input("input here: ")
             # thing = self.send_request(user_input)
 
             self.clock.tick(60)
-            thing = self.send_request("refresh")
+            # self.clock.tick(1)  # temporary, to reduce the number of messages
+            thing = self.send_request(message)
 
             print(thing)
+
+            # draw things
+            self.draw()
+            pygame.display.update()
+
+    class Button:
+        """
+        `Button` inner class for the UI.
+        """
+
+        BUTTON_FONT: pygame.freetype.Font = pygame.freetype.Font(
+            "../res/font/robotoRegular.ttf", 48)
+
+        def __init__(self, text: str, pos: tuple[int, int], size: tuple[int, int],
+                     fg: tuple[int, int, int], bg: tuple[int, int, int],
+                     font: pygame.freetype.Font) -> None:
+            """
+            Constructor.
+
+            Parameters
+            ---
+            `text: str` - button text.
+            `pos: tuple[int, int]` - position of the button on the window.
+            `size: tuple[int, int]` - width and height.
+            `fg: tuple[int, int, int]` - RGB color of the text.
+            `bg: tuple[int, int, int]` - RGB color of the background.
+
+            Returns
+            ---
+            `None`
+            """
+            self.text: str = text
+            self.pos: tuple[int, int] = pos
+            self.size: tuple[int, int] = size
+            self.rect = pygame.Rect(*self.pos, *self.size)
+            self.fg: tuple[int, int, int] = fg
+            self.bg: tuple[int, int, int] = bg
+            self.font: pygame.font.Font = font
+
+        def draw(self, window: pygame.Surface):
+            """
+            Draw this button to the specified window.
+
+            Parameters
+            ---
+            `window: pygame.Surface` - window to draw on.
+
+            Returns
+            ---
+            `None`
+            """
+            fg = self.fg
+            bg = self.bg
+
+            # reverse colors when hovered
+            if self.rect.collidepoint(pygame.mouse.get_pos()):
+                fg = self.bg
+                bg = self.fg
+            button_rect = pygame.draw.rect(
+                window, bg, self.rect)
+            # add outline when hovered
+            if self.rect.collidepoint(pygame.mouse.get_pos()):
+                pygame.draw.rect(window, fg, self.rect, width=2)
+
+            text_surface = self.font.render(
+                self.text, fg, bg)[0]
+            text_rect = text_surface.get_rect(center=button_rect.center)
+            window.blit(text_surface, text_rect)
+
+        def check_clicked(self) -> bool:
+            """
+                Check if this button has been clicked.
+
+                Parameters
+                ---
+                (no parameters)
+
+                Returns
+                ---
+                `bool` - `True` if clicked, `False` otherwise
+                """
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            return (self.pos[0] <= mouse_x <= self.pos[0] + self.size[0] and
+                    self.pos[1] <= mouse_y <= self.pos[1] + self.size[1])
 
 
 def main() -> None:
